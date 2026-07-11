290b1875685c5619aceab5f41f808f1fc0ead8e7
badragon <badragon20100413@gmail.com>
2026-07-11 20:38:02 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -12,5 +12,5 @@ export const GITHUB_CONFIG = {
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'bdg-blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '4272325',
-	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
+	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || '-',
 } as const
