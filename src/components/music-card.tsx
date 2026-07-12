f80b3624fd69c0c9f8a2d3e81ab90901be06a57b
badragon <badragon20100413@gmail.com>
2026-07-12 11:30:10 +0800

    Update music-card.tsx

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -12,7 +12,7 @@ import { Pause } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-const MUSIC_FILES = ['/music/music.mp3']
+const MUSIC_FILES = ['/music/Komorebi.mp3']
 
 export default function MusicCard() {
 	const pathname = usePathname()
@@ -158,7 +158,7 @@ export default function MusicCard() {
 				<MusicSVG className='h-8 w-8' />
 
 				<div className='flex-1'>
-					<div className='text-secondary text-sm'>Windy Hill</div>
+					<div className='text-secondary text-sm'>Komorebi</div>
 
 					<div className='mt-1 h-2 rounded-full bg-white/60'>
 						<div className='bg-linear h-full rounded-full transition-all duration-300' style={{ width: `${progress}%` }} />
